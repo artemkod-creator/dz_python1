b0be24cc8b7dc680bc797b75da98efe7e3554c8b
artemkod-creator <tuxagame2210@gmail.com>
2024-10-08 23:08:53 +0300

    task1

---
+++ task1.py
@@ -0,0 +1,18 @@
+# todo: Определить в коде переменные:
+# 1. Целочисленного типа
+# 2. Вещественного типа
+# 3. Логического типа
+# 4. Строкового типа
+# 5. Пустого типа
+# Вывести их типы.
+
+cel_Chislo = int(input("Введите целочисленное число:"))
+print(type(cel_Chislo))
+vech_Chislo = float(input("Введите вещественное число:"))
+print(type(vech_Chislo))
+log_Tip = bool(input("Введите True или False: "))
+print(type(log_Tip))
+strok_Tip = str(input("Введите любую строку: "))
+print(type(strok_Tip))
+strok_Tip1 = str(input("Введите пустую строку: "))
+print(type(strok_Tip1))
