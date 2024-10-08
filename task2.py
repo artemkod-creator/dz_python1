d1e7e1c1459b1d9d943eadcc530284efd29145c2
artemkod-creator <tuxagame2210@gmail.com>
2024-10-08 23:16:40 +0300

    task2

---
+++ task2.py
@@ -0,0 +1,32 @@
+# todo: Преобразуйте переменную age и foo в число
+# age = "23"
+# foo = "23abc"
+# Преобразуйте переменную age в Boolean
+# age = "123abc"
+# Преобразуйте переменную flag в Boolean
+# flag = 1
+# Преобразуйте значение в Boolean
+# str_one = "Privet"
+# str_two = ""
+# Преобразуйте значение 0 и 1 в Boolean
+# Преобразуйте False в строку
+age="23"
+age=int(age)
+foo="23abc"
+foo=int(foo[:2])
+
+age_1="123abc"
+age_1=bool(age_1)
+
+flag=1
+flag=bool(flag)
+
+str_one="Privet"
+str_two= ""
+str_one=bool(str_one)
+str_two=bool(str_two)
+
+bool(1)
+bool(0)
+
+str(False)
