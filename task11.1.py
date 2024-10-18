c5a7d07c6b4b283a49c2d392c574ab8caee84f22
artemkod-creator <tuxagame2210@gmail.com>
2024-10-19 01:25:07 +0300

    task11.py

---
+++ task11.1.py
@@ -0,0 +1,10 @@
+year=int(input("Введите год: "))
+if year<=100:
+      print(f"В {year} году было 1 столетие")
+      exit(0)
+if int(year)%10 + int(year)%100==0:
+    stoletie=year//100
+    print(f"В {year} году было {stoletie} столетие")
+else:
+    stoletie = year//100+1
+    print(f"В {year} году было {stoletie} столетие")
