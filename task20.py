2dbf60a47479e3e750a719c36ae2a345716863c8
artemkod-creator <tuxagame2210@gmail.com>
2024-10-24 01:42:02 +0300

    task14.py

--- task20.py
+++ task20.py
@@ -1,9 +1,6 @@
 f=open(' import_this.txt','r')
-str_str=[]
 str_s=""
-for i in f.readlines():
-    str_str.append(i)
-for i in str_str[::-1]:
+for i in f.readlines()[::-1]:
     str_s+=i
 print(str_s)
 
