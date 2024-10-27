77551d700cf70a15a1ceae724f7dc170964b49f3
artemkod-creator <tuxagame2210@gmail.com>
2024-10-27 21:35:26 +0300

    task16

---
+++ task16.py
@@ -0,0 +1,37 @@
+users = [{'login': 'Piter', 'age': 23, 'group': "admin"},
+         {'login': 'Ivan',  'age': 10, 'group': "guest"},
+         {'login': 'Dasha', 'age': 30, 'group': "master"},
+         {'login': 'Fedor', 'age': 13, 'group': "guest"}]
+# Рассматриваем сортировку по возрасту. Я бы в каждом словаре сравнивал заданной значений со значением ключа 'age'
+# если значение 'age'>заданного то выводить весь словарь
+sortirovka=int(input("Выберите число для сортировки, где\n1. По возрасту\n"
+                     "2. По первой букве\n"
+                     "3. По группе\n "
+                     "Вводите тут : "))
+match sortirovka:
+    case 1:
+        age_=int(input("Введите возраст: "))
+        n=0
+        while n<len(users):
+            dict_ = users[n].get('age')
+            if age_<dict_:
+                print(users[n])
+            n+=1
+    case 2:
+        name_=(input("Введите первую букву пользователя: "))
+        n=0
+        while n<len(users):
+            dict_ = users[n].get('login')
+            if dict_[0]==name_:
+                print(users[n])
+            n+=1
+    case 3:
+        group_=(input("Введите группу: "))
+        n=0
+        while n<len(users):
+            dict_=users[n].get('group')
+            if group_==dict_:
+                print(users[n])
+            n+=1
+
+
